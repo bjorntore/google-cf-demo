78552e7c755150760c35a568a4c7f89e50687510
Bjørn Tore Gjerde <btgjerde@gmail.com>
2017-12-11 20:35:05 +0100

    Datastore integration

--- StoreTheThing/index.js
+++ StoreTheThing/index.js
@@ -1,21 +1,40 @@
-function handleGET (req, res) {
-    res.status(200).send('Hello World!');
+const datastore = require('@google-cloud/datastore')();
+const key = datastore.key('Thing');
+
+function getTheThings(req, res) {
+  let query = datastore.createQuery(key);
+  res.status(200).send(JSON.stringify(query));
+}
+
+function saveNewThing(req, res) {
+  let theThing = req.body.theThing;
+
+  datastore.save({
+    key: key,
+    description: theThing
+  }, function (err) {
+    res
+      .status(400)
+      .send({
+        error: 'Failed to save thing with key ' + key.path
+      });
+  });
+
+  res.status(200).send('Saved the thing with key ' + key.path);
+}
+
+exports.storeTheTing = function (req, res) {
+  switch (req.method) {
+    case 'GET':
+      getTheThings(req, res);
+      break;
+    case 'POST':
+      saveNewThing(req, res);
+      break;
+    default:
+      res
+        .status(500)
+        .send({error: 'Something blew up!'});
+      break;
   }
-  
-  function handlePOST (req, res) {
-    res.status(403).send('Forbidden!');
-  }
-  
-  exports.storeTheTing = function (req, res) {
-    switch (req.method) {
-      case 'GET':
-        handleGET(req, res);
-        break;
-      case 'POST':
-        handlePOST(req, res);
-        break;
-      default:
-        res.status(500).send({ error: 'Something blew up!' });
-        break;
-    }
-  };
+};
