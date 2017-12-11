60486ba2c0be351dfbcaf541a24c47417d10acce
Bjørn Tore Gjerde <btgjerde@gmail.com>
2017-12-11 19:22:26 +0100

    Return just the name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.tellMeMyName = function (req, res) {
-    let name = JSON.stringify(req.body);
+    let name = req.body.name;
 	let content =  "Your name is " + name;
     res.status(200).send(content);
 }
