d0e4404e48222781618592c6ae273c6488b3ec41
Bjørn Tore Gjerde <btgjerde@gmail.com>
2017-12-11 21:12:56 +0100

    Return the actual entities

--- StoreTheThing/index.js
+++ StoreTheThing/index.js
@@ -3,7 +3,13 @@ var key = datastore.key(['Thing']);
 
 function getTheThings(req, res) {
   let query = datastore.createQuery(key);
-  res.status(200).send(JSON.stringify(query));
+
+  datastore.runQuery(query, function (err, entities) {
+    res
+      .status(200)
+      .send(JSON.stringify(entities));
+  });
+
 }
 
 function saveNewThing(req, res) {
@@ -11,15 +17,19 @@ function saveNewThing(req, res) {
   var data = {
     data: 'Google'
   };
-  
+
   datastore.save({
     key: key,
     data: data
-  }, function(err) {
+  }, function (err) {
     if (!err) {
-      res.status(200).send("All is good.");
-    }else{
-      res.status(400).send({error: err});
+      res
+        .status(200)
+        .send("All is good.");
+    } else {
+      res
+        .status(400)
+        .send({error: err});
     }
   });
 }
